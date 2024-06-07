7378db12f3761d72309798e98fe2ba8e9bea9c35
KanishkGoell <goel.kanishk21@gmail.com>
2024-06-08 01:05:57 +0530

    added vercel.json 5

--- frontend/api/getTable.js
+++ frontend/api/getTable.js
@@ -1,14 +1,14 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
 
-export default function handler(req, res) {
-  const jsonFilePath = path.join(process.cwd(), 'backend', 'data.json');
+export default (req, res) => {
+  const jsonFilePath = path.join(process.cwd(), 'backend/data.json');
   fs.readFile(jsonFilePath, 'utf8', (err, data) => {
     if (err) {
       console.error("Error reading JSON file:", err);
       res.status(500).send("Internal Server Error");
       return;
     }
-    res.status(200).json(JSON.parse(data));
+    res.json(JSON.parse(data));
   });
-}
+};
